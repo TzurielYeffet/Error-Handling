841d767640c4cd00cae5bc63eca3cf5b65701e1d
TzurielYeffet <Tzuriel66ye@yahoo.com>
2025-07-16 00:54:23 +0300

    add functions to contactService

--- Contact Manager/utils/fileUtils.js
+++ Contact Manager/utils/fileUtils.js
@@ -1,59 +1,58 @@
 const fs = require("fs");
 const path = "./contacts.json";
 
-const load = function (email) {
-  let objArr = loadFile();
-  for (obj of objArr) {
-    if (obj.email === email) {
-      console.log("✗ Error: Contact with this email already exists");
-    } else {
-        console.error(`✗ Error: No contact found with email: ${email}`);
-    }
-  }
-};
+// const load = function () {
+//   let objArr = loadFile();
+//   for (obj of objArr) {
+//     if (obj.email === email) {
+//       return objArr;
+//     } else {
+//       console.error(`✗ Error: No contact found with email: ${email}`);
+//     }
+//   }
+// };
 
+//this should only save the data
 const save = function (obj) {
-  let objArr = loadFile();
-  if (objArr.length > 0) {
-    console.log(`✓ Loaded ${objArr.length} contacts`);
-    for (obj of objArr) {
-      if (obj.email === email) {
-        console.error("✗ Error: Contact with this email already exists");
-      } else {
-        fs.writeFileSync(path, JSON.stringify(obj, null, 2));
-      }
-    }
+  try {
+    fs.writeFileSync(obj, null, 2);
+    console.log(`✓ Contact added: ${newContact.name}`);
+    console.log(`✓ Contacts saved to ${path} `);
     return true;
+  } catch (err) {
+    console.error("✗ Failed to save contact:", err.message);
+    return false;
   }
-  return false;
 };
 
-const loadFile = function () {
+const load = function () {
   console.log("\nLoading contacts from contacts.json...\n");
   if (!fs.existsSync(path)) {
     console.log("✗ File not found - creating new contact list");
     return [];
   }
 
-  try{
-      const data = fs.readFileSync(path, "utf-8");
-      return JSON.parse(data);
-  }catch(err){
-      switch (err.code) {
-        case "ENOENT":
-          console.error("File or directory not found");
-          break;
-        case "ENOSPC":
-          console.error("Disk is full");
+  try {
+    const data = fs.readFileSync(path, "utf-8");
+    if (data.length > 0) {
+      console.log(`✓ Loaded ${objArr.length} contacts`);
+    }
+    return JSON.parse(data);
+  } catch (err) {
+    switch (err.code) {
+      case "ENOENT":
+        console.error("File or directory not found");
+        break;
+      case "ENOSPC":
+        console.error("Disk is full");
 
-        default:
-          console.error(`✗ Some other error: ${err.code}`);
+      default:
+        console.error(`✗ Some other error: ${err.code}`);
 
-          break;
-      }
-      return [];
+        break;
+    }
+    return [];
   }
-}
+};
 
-
-module.exports = {save,load}
+module.exports = { save, load };
