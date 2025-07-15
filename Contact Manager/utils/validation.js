198849923f11b5b85d7e1dbcbed242364989abef
TzurielYeffet <Tzuriel66ye@yahoo.com>
2025-07-15 14:53:52 +0300

    add conditions for email validation and implemeted addContact and deleteContact dunctions

--- Contact Manager/utils/validation.js
+++ Contact Manager/utils/validation.js
@@ -4,9 +4,24 @@ const validatePhone = function(phone){
 }
 
 const validateEmail = function(email){
-    return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-z]{2,}$/.test(email);
-}
+if (!email) {
+    console.log("✗ Error: Email cannot be empty");
+    return false;
+  }
 
+  if (!email.includes("@")) {
+    console.log("✗ Error: Email must contain @ symbol");
+    return false;
+  }
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  
+  if (!emailRegex.test(email)) {
+    console.log("✗ Error: Invalid email format");
+    return false;
+  }}
+
+
+module.exports = {validateEmail,validatePhone};
 // console.log(validateEmail("tuzriel66ye@yahoo.com"));
 // console.log(validateEmail("tuzriel66yeyahoo.com"));
 // console.log(validatePhone("1234567890"));
